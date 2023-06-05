3977a063f20d916a268721f926b6a024758aa4ac
Bryan Revelo <bryanreveloyagual@outlook.com>
2023-06-04 21:19:37 -0500

    Initial commit

---
+++ figura_geometrica.py
@@ -0,0 +1,23 @@
+from figura_geometrica import FiguraGeometrica
+
+
+class Triangulo(FiguraGeometrica):
+
+    def __init__(self, lado: float = 0.0):
+        super().__init__(ancho=lado, alto=lado)
+
+    def __str__(self):
+        return f'triangulo [lado = {self.alto}]'
+
+    def calcular_area(self):
+        return (self.alto * self.ancho) / 2
+
+    def calcular_perimetro(self):
+        return 3 * self.alto
+
+
+if __name__ == '__main__':
+    c1 = Triangulo(lado=3)
+    print(c1)
+    print(f'el area del tiangulo es: {c1.calcular_area()}')
+    print(f'el perimetro del tiangulo es: {c1.calcular_perimetro()}')
